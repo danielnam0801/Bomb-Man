734138fb620b3b28c33ed02eb4d7ecd9fae3d77a
Nam junseong <danielnam0801@gmail.com>
2023-05-16 00:53:49 +0900

    PlayerMovement

---
+++ Assets/01.Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        
+    }
+}
